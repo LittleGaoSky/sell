b4fdd052e192751575b13c4b0f547d30a038159e
LittleGaoSky <1414047483@qq.com>
2017-08-10 17:13:24 +0800

    准备工作和页面骨架开发

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <v-header></v-header>
+    <div class="tab border-1px">
+      <router-link to="/goods" class="tab-item">商品</router-link>
+      <router-link to="/ratings" class="tab-item">评论</router-link>
+      <router-link to="/seller" class="tab-item">商家</router-link>
+    </div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  import header from '@/components/header/header.vue';
+
+  export default {
+    components: {
+      'v-header': header
+    }
+  };
+</script>
+
+<style lang="stylus" type="text/stylus" rel="stylesheet/stylus">
+  @import "./common/stylus/mixin.styl"
+  .tab
+    display: flex
+    width: 100%
+    height: 40px
+    line-height: 40px
+    border-1px(rgba(7, 17, 27, 0.1))
+    .tab-item
+      flex: 1
+      text-align: center
+    & > a
+      font-size: 14px
+      color: rgb(77, 85, 93)
+      &.router-link-active
+        color: rgb(240, 20, 20)
+</style>
