0e64c2fffd74fb15856f13d40783f9da9199be8f
LittleGaoSky <1414047483@qq.com>
2017-08-11 19:13:06 +0800

    小标题自适应经典flex布局实现

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -40,6 +40,11 @@
           <div class="star-wrapper">
             <star :size="48" :score="seller.score"></star>
           </div>
+          <div class="title">
+            <div class="line"></div>
+            <div class="text">优惠信息</div>
+            <div class="line"></div>
+          </div>
         </div>
       </div>
       <div class="detail-close" @click="showDetail()">
@@ -207,14 +212,27 @@
         .detail-main
           margin-top: 64px
           padding-bottom: 64px
-          text-align: center
           .name
             line-height: 16px
             font-size: 16px
             font-weight: 700
+            text-align: center
           .star-wrapper
-            margin-top: 16px
+            margin-top: 18px
             padding: 2px 0
+            text-align: center
+          .title
+            display: flex
+            width: 80%
+            margin: 30px auto 24px auto
+            .line
+              flex: 1
+              position: relative
+              top: -6px
+              border-bottom: 1px solid rgba(255,255,255,0.2)
+            .text
+              padding: 0 12px
+              font-size: 14px
       .detail-close
         position: relative
         width: 32px
